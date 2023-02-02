30aa97db20a7ecc939891c29fce4bf44900bc10b
Gonzalo Guzman <gonzarg2408@gmail.com>
2023-02-02 00:06:08 -0300

    correcciones

--- js/carrito.js
+++ js/carrito.js
@@ -1,39 +1,40 @@
-let productosEnCarrito = localStorage.getItem("productos-en-carrito");
-productosEnCarrito = JSON.parse(productosEnCarrito);
+let productosEnCarrito = localStorage.getItem("productos-en-carrito")
+productosEnCarrito = JSON.parse(productosEnCarrito)
 
-const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
-const contenedorCarritoProductos = document.querySelector("#carrito-productos");
-const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
-const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
-let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
-const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
-const contenedorTotal = document.querySelector("#total");
-const botonComprar = document.querySelector("#carrito-acciones-comprar");
-
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio")
+const contenedorCarritoProductos = document.querySelector("#carrito-productos")
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones")
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado")
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar")
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar")
+const contenedorTotal = document.querySelector("#total")
+const botonComprar = document.querySelector("#carrito-acciones-comprar")
 
 function cargarProductosCarrito() {
     if (productosEnCarrito && productosEnCarrito.length > 0) {
 
-        contenedorCarritoVacio.classList.add("disabled");
-        contenedorCarritoProductos.classList.remove("disabled");
-        contenedorCarritoAcciones.classList.remove("disabled");
-        contenedorCarritoComprado.classList.add("disabled");
-    
-        contenedorCarritoProductos.innerHTML = "";
+        contenedorCarritoVacio.classList.add("disabled")
+        contenedorCarritoProductos.classList.remove("disabled")
+        contenedorCarritoAcciones.classList.remove("disabled")
+        contenedorCarritoComprado.classList.add("disabled")
+
+        contenedorCarritoProductos.innerHTML = ""
     
         productosEnCarrito.forEach(producto => {
     
-            const div = document.createElement("div");
-            div.classList.add("carrito-producto");
+            const div = document.createElement("div")
+            div.classList.add("carrito-producto")
             div.innerHTML = `
                 <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
                 <div class="carrito-producto-titulo">
                     <small>Título</small>
-                    <h3>${producto.titulo}</h3>
+                    <h3 class="titulo">${producto.titulo}</h3>
                 </div>
-                <div class="carrito-producto-cantidad">
+                <div
+                    class="carrito-producto-cantidad">
                     <small>Cantidad</small>
-                    <p>${producto.cantidad}</p>
+                    <input class="carrito-producto-cantidad-input carritoProductoCantidad" type="number"
+                    min="1" value=${producto.cantidad}>
                 </div>
                 <div class="carrito-producto-precio">
                     <small>Precio</small>
@@ -46,22 +47,25 @@ function cargarProductosCarrito() {
                 <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
             `;
     
-            contenedorCarritoProductos.append(div);
+            contenedorCarritoProductos.append(div)
+            div.querySelector('.carritoProductoCantidad').
+            addEventListener('change', sumaCantidad)
+
         })
     
-    actualizarBotonesEliminar();
-    actualizarTotal();
+    actualizarBotonesEliminar()
+    actualizarTotal()
 	
     } else {
-        contenedorCarritoVacio.classList.remove("disabled");
-        contenedorCarritoProductos.classList.add("disabled");
-        contenedorCarritoAcciones.classList.add("disabled");
-        contenedorCarritoComprado.classList.add("disabled");
+        contenedorCarritoVacio.classList.remove("disabled")
+        contenedorCarritoProductos.classList.add("disabled")
+        contenedorCarritoAcciones.classList.add("disabled")
+        contenedorCarritoComprado.classList.add("disabled")
     }
 
 }
 
-cargarProductosCarrito();
+cargarProductosCarrito()
 
 function actualizarBotonesEliminar() {
     botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
@@ -72,24 +76,58 @@ function actualizarBotonesEliminar() {
 }
 
 function eliminarDelCarrito(e) {
+    Toastify({
+        text: "Producto eliminado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #961818, #d40505)",
+          borderRadius: "2rem",
+          textTransform: "uppercase"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast();
 
-    const idBoton = e.currentTarget.id;
-    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    const idBoton = e.currentTarget.id
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton)
     
-    productosEnCarrito.splice(index, 1);
-    cargarProductosCarrito();
+    productosEnCarrito.splice(index, 1)
+    cargarProductosCarrito()
 
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
 
 }
+function sumaCantidad(e){
+    const input = e.target
+    const div = input.closest(".carrito-producto")
+    const titulo = div.querySelector('.titulo').textContent
+    productosEnCarrito.forEach(producto => {
+        if(producto.titulo.trim() === titulo){
+          input.value < 1 ?  (input.value = 1) : input.value
+          producto.cantidad = input.value
+          
+        }
+      })
+      localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+      cargarProductosCarrito()
+      actualizarTotal()
+      
+}
 
-botonVaciar.addEventListener("click", vaciarCarrito);
+botonVaciar.addEventListener("click", vaciarCarrito)
 function vaciarCarrito() {
 
     Swal.fire({
         title: '¿Estás seguro?',
         icon: 'question',
-        html: `Se van a borrar ${productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0)} productos.`,
+        html: `Se van a borrar todos tus productos`,
         showCancelButton: true,
         focusConfirm: false,
         confirmButtonText: 'Sí',
@@ -97,27 +135,27 @@ function vaciarCarrito() {
     }).then((result) => {
         if (result.isConfirmed) {
             productosEnCarrito.length = 0;
-            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-            cargarProductosCarrito();
+            localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
+            cargarProductosCarrito()
         }
       })
 }
 
 
 function actualizarTotal() {
-    const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
-    total.innerText = `$${totalCalculado}`;
+    const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0)
+    total.innerHTML = `$${totalCalculado}`
 }
 
-botonComprar.addEventListener("click", comprarCarrito);
+botonComprar.addEventListener("click", comprarCarrito)
 function comprarCarrito() {
 
     productosEnCarrito.length = 0;
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
     
-    contenedorCarritoVacio.classList.add("disabled");
-    contenedorCarritoProductos.classList.add("disabled");
-    contenedorCarritoAcciones.classList.add("disabled");
-    contenedorCarritoComprado.classList.remove("disabled");
+    contenedorCarritoVacio.classList.add("disabled")
+    contenedorCarritoProductos.classList.add("disabled")
+    contenedorCarritoAcciones.classList.add("disabled")
+    contenedorCarritoComprado.classList.remove("disabled")
 
 }
--- js/index.js
+++ js/index.js
@@ -1,33 +1,33 @@
-let productos = [];
+let productos = []
 
 fetch("./js/productos.json")
     .then(response => response.json())
     .then(data => {
         productos = data;
-        cargarProductos(productos);
+        cargarProductos(productos)
     })
 
 
-const contenedorProductos = document.querySelector("#contenedor-productos");
-const botonesCategorias = document.querySelectorAll(".boton-categoria");
-const tituloPrincipal = document.querySelector("#titulo-principal");
-let botonesAgregar = document.querySelectorAll(".producto-agregar");
-const numerito = document.querySelector("#numerito");
+const contenedorProductos = document.querySelector("#contenedor-productos")
+const botonesCategorias = document.querySelectorAll(".boton-categoria")
+const tituloPrincipal = document.querySelector("#titulo-principal")
+let botonesAgregar = document.querySelectorAll(".producto-agregar")
+
 
 
 botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
-    aside.classList.remove("aside-visible");
+    aside.classList.remove("aside-visible")
 }))
 
 
 function cargarProductos(productosElegidos) {
 
-    contenedorProductos.innerHTML = "";
+    contenedorProductos.innerHTML = ""
 
     productosElegidos.forEach(producto => {
 
-        const div = document.createElement("div");
-        div.classList.add("producto");
+        const div = document.createElement("div")
+        div.classList.add("producto")
         div.innerHTML = `
             <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
             <div class="producto-detalles">
@@ -35,72 +35,66 @@ function cargarProductos(productosElegidos) {
                 <p class="producto-precio">$${producto.precio}</p>
                 <button class="producto-agregar" id="${producto.id}">Agregar</button>
             </div>
-        `;
+        `
 
-        contenedorProductos.append(div);
+        contenedorProductos.append(div)
     })
 
-    actualizarBotonesAgregar();
+    actualizarBotonesAgregar()
 }
 
 
-botonesCategorias.forEach(boton => {
-    boton.addEventListener("click", (e) => {
-
-        botonesCategorias.forEach(boton => boton.classList.remove("active"));
-        e.currentTarget.classList.add("active");
-
-        if (e.currentTarget.id != "todos") {
-            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
-            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
-            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-            cargarProductos(productosBoton);
-        } else {
-            tituloPrincipal.innerText = "Todos los productos";
-            cargarProductos(productos);
-        }
-
-    })
-});
 
 function actualizarBotonesAgregar() {
-    botonesAgregar = document.querySelectorAll(".producto-agregar");
+    botonesAgregar = document.querySelectorAll(".producto-agregar")
 
     botonesAgregar.forEach(boton => {
-        boton.addEventListener("click", agregarAlCarrito);
-    });
+        boton.addEventListener("click", agregarAlCarrito)
+    })
 }
 
-let productosEnCarrito;
+let productosEnCarrito
 
-let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito")
 
 if (productosEnCarritoLS) {
-    productosEnCarrito = JSON.parse(productosEnCarritoLS);
-    actualizarNumerito();
+    productosEnCarrito = JSON.parse(productosEnCarritoLS)
 } else {
-    productosEnCarrito = [];
+    productosEnCarrito = []
 }
 
 function agregarAlCarrito(e) {
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #961818, #d40505)",
+          borderRadius: "2rem",
+          textTransform: "uppercase"
+        },
+        offset: {
+            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
+          },
+        onClick: function(){} // Callback after click
+      }).showToast()
 
-    const idBoton = e.currentTarget.id;
-    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    const idBoton = e.target.id
+    const productoAgregado = productos.find(producto => producto.id === idBoton)
 
     if(productosEnCarrito.some(producto => producto.id === idBoton)) {
-        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-        productosEnCarrito[index].cantidad++;
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton)
+        productosEnCarrito[index].cantidad++
     } else {
-        productoAgregado.cantidad = 1;
-        productosEnCarrito.push(productoAgregado);
+        productoAgregado.cantidad = 1
+        productosEnCarrito.push(productoAgregado)
     }
 
-    actualizarNumerito();
 
-    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-}
 
-function actualizarNumerito() {
-    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
-    numerito.innerText = nuevoNumerito;
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito))
 }
